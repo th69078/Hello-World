0c6a1438389204770dcaf62108e6c0b24b0a0d64
th69078 <69616936+th69078@users.noreply.github.com>
2020-08-12 21:37:48 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.print("One of my favorite hobbies is practicng Competitve Smash Ultimate.");
+		System.out.print(" The college I want to go to is UCSC.");
+		System.out.print(" My favorite genre of games are JRPGs and Fighting Games.");
+		System.out.print(" I would like to major in Art for Entertainment.");
+		System.out.print(" Another hobby I enjoy is listening to music.");
+	}
+}
